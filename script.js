d4aa939460c0434cecaed547b7a802c4b9675b8f
anastasiatomuz <101012519+anastasiatomuz@users.noreply.github.com>
2023-11-08 10:47:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+
+
+//edmond worked on maps and foundation for randomize; 
+//anastasia worked on cardClicked and checkMatch function to make sure there were no bugs when double clicking and that all cards flipped over correctly
+
 //important variables
 let recentTries = []; //this is what we will use to store the current match (actual img objects)
 let score = 0; //keeps track of score
 let setsMatched = 0;//keeps track of how many sets the user matched correctly; max 8
 let scoreText = document.getElementById("score");
+
+let reset = document.getElementById("reset");
 const imgMap = new Map();
 let cardsFound = [];//array to store the cards that have already been found
 const instructions = document.getElementById("instructions");
@@ -22,11 +29,7 @@ const colorsArray = [
   "blue.jpg", "purple.png", "pink.png", "brown.png"];
 
 
-// //event listeners
-// reset.addEventListener("click", () => {resetGame()});
-
-
-//Map of img objects to r,c array
+//Map of img objects to r,c array adding EventListeners
 for (let r = 0; r < 4; r ++){
   for (let c = 0; c < 4; c ++){
     let temp = document.getElementById("card" + r + c); //get the img object from html
@@ -39,13 +42,12 @@ for (let r = 0; r < 4; r ++){
 
 
 function cardClicked(imgObj){
-  
   if (recentTries.length == 2 || (recentTries.length != 0 && recentTries[0]==imgObj) || cardsFound.includes(imgObj)){ //https://www.w3schools.com/jsref/jsref_includes_array.asp
       //do nothing while the two cards are still being displayed before they flip back over
       //do nothing if the user clicked the same card twice
   } else {
     recentTries.push(imgObj); //store the img object of the image clicked
-    score ++; //updates score each time the user gusses
+    score++; //updates score each time the user gusses
     scoreText.innerHTML = score;
 
     let imagecoor = imgMap.get(imgObj); //gets an array of [r,c] where imagesrc link is stored in keyGrid
@@ -58,7 +60,6 @@ function cardClicked(imgObj){
       gameWon();
     }
   }
-  console.log(cardsFound);
 }
 
 /*function that checks if the two recent tries have matching cards*/
@@ -68,28 +69,24 @@ function checkMatch(){
 
   let image1 = keyGrid[img1coor[0]][img1coor[1]];
   let image2 = keyGrid[img2coor[0]][img2coor[1]]
-  console.log(image1[0]);
 
-  console.log(image2[0]);
-
-  console.log(typeof image1);
   if (keyGrid[img1coor[0]][img1coor[1]] === keyGrid[img2coor[0]][img2coor[1]]){//checks that img1 = img2 | imgxcoor[0] returns row, imgxcoor[1] returns column
     cardsFound.push(recentTries[0]);
     cardsFound.push(recentTries[1]);
     setsMatched++;
-    recentTries = [];//empties array that stores img objects for keeping track of guesses
+    recentTries = []; //empties array that stores img objects for keeping track of guesses
   } else {
 
     /*
-    
     sleep function doesn't exist in JS, so you have to make your own
     credits to Dan Dascalescu 
     https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep/39914235#39914235
     */
+   
     function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
   
+    //after delay of 1 second resets cards if no match; empties array
     async function demo() {
-      console.log("inside async");
       await sleep(1000);
       recentTries[0].src = "blank.jpg";
       recentTries[1].src = "blank.jpg";
@@ -102,6 +99,7 @@ function checkMatch(){
 function gameWon(){
   instructions.innerHTML = `You won! It took you ${score} clicks.
   Click the reset button below to play again.`;
+  reset.innerHTML = "Play Again";
 }
 
 //function that resets the game
@@ -112,6 +110,7 @@ function resetGame(){
     obj.src = "blank.jpg";
   }
   scoreText.innerHTML = 0;
+  reset.innerHTML = "Reset";
   instructions.innerHTML = `Each of the gray boxes below is a flipped-down 
   card. To flip it over, simply click on it. Your job is to flip over two cards
 at a time such that the pair have the same color. To win the game, you must flip
@@ -120,14 +119,13 @@ over all 8 pairs. Good luck!`;
 }
 
 function randomize() {
-  
   let string = "";
   copyOfList = [...colorsArray];
  
   for (let r = 0; r < 4; r ++){
     let substring = "";
        for (let c = 0; c < 4; c ++){
-        let toMove = copyOfList.splice(Math.floor(Math.random() * copyOfList.length), 1).toString(); //https://www.w3schools.com/js/js_json_stringify.asp#:~:text=Stringify%20a%20JavaScript%20Object&text=Use%20the%20JavaScript%20function%20JSON,string%20following%20the%20JSON%20notation.
+        let toMove = copyOfList.splice(Math.floor(Math.random() * copyOfList.length), 1).toString();
       
         keyGrid[r][c] = toMove;
      
@@ -136,11 +134,9 @@ function randomize() {
     
     string += substring + "\n";
   }
-  console.log(string);
 }
 
 /*
-
 reason for calling cardClicked inside an anonymous function: 
 you will automatically have the function run if the function inside the addEventListener has parameters, so you need to prevent that 
 further explanation: https://stackoverflow.com/questions/30354136/function-call-with-parameters-to-addeventlistener-inside-a-loop 
